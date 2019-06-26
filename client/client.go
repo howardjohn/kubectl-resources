6edd9c3f0b3d13a286100fe715c8438781e477a5
John Howard <howardjohn@google.com>
2019-06-26 07:55:58 -0700

    Get current usage

--- client/client.go
+++ client/client.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/tools/clientcmd"
+	metrics "k8s.io/metrics/pkg/client/clientset/versioned"
 )
 
 type Args struct {
@@ -17,7 +17,7 @@ type Args struct {
 func createClient(kubeconfig string) (*kubernetes.Clientset, error) {
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to get kubeconfig: %v", err)
+		return nil, fmt.Errorf("failed to get kubeconfig: %v", err)
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
@@ -28,15 +28,64 @@ func createClient(kubeconfig string) (*kubernetes.Clientset, error) {
 	return clientset, nil
 }
 
+type Resource struct {
+	Request int
+	Limit   int
+	Usage   int
+}
+
+type ContainerResource struct {
+	Cpu    *Resource
+	Memory *Resource
+}
+type PodResource struct {
+	Name       string
+	Namespace  string
+	Node       string
+	Containers map[string]*ContainerResource
+}
+
 func Run(args *Args) error {
-	client, err := createClient(args.KubeConfig)
+	//client, err := createClient(args.KubeConfig)
+	//if err != nil {
+	//	return fmt.Errorf("failed to create client: %v", err)
+	//}
+
+	config, err := clientcmd.BuildConfigFromFlags("", args.KubeConfig)
 	if err != nil {
-		return fmt.Errorf("failed to create client: %v", err)
+		return fmt.Errorf("failed to get kubeconfig: %v", err)
 	}
-	ns, err := client.CoreV1().Namespaces().Get("default", metav1.GetOptions{})
+
+	metricsclient, err := metrics.NewForConfig(config)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create metrics client: %v", err)
 	}
-	fmt.Printf("Got namespace: %v\n", ns)
+
+	podlist, err := metricsclient.MetricsV1beta1().PodMetricses(args.Namespace).List(metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to get pod metrics: %v", err)
+	}
+	if podlist.Continue != "" {
+		fmt.Println("Continue:", podlist.Continue)
+	}
+
+	res := map[string]*PodResource{}
+	for _, pod := range podlist.Items {
+		res[pod.Name] = &PodResource{
+			Name:       pod.Name,
+			Namespace:  pod.Namespace,
+			Containers: make(map[string]*ContainerResource),
+		}
+		for _, container := range pod.Containers {
+			res[pod.Name].Containers[container.Name] = &ContainerResource{
+				Cpu: &Resource{
+					Usage: int(container.Usage.Cpu().MilliValue()),
+				},
+				Memory: &Resource{
+					Usage: int(container.Usage.Memory().MilliValue()),
+				},
+			}
+		}
+	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.12
 
 require (
 	github.com/spf13/cobra v0.0.5
-	k8s.io/apimachinery v0.0.0-20190624085041-961b39a1baa0
 	k8s.io/client-go v0.0.0-20190624085356-2c6e35a5b9cf
+	k8s.io/metrics v0.0.0-20190624090658-79adfaea0da2
 )
