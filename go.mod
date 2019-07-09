8989b15bd3268569587ada6644eb4e23e436cc86
John Howard <howardjohn@google.com>
2019-07-08 21:02:59 -0700

    Switch makefile for magefile

--- go.mod
+++ go.mod
@@ -3,9 +3,15 @@ module github.com/howardjohn/kubectl-resources
 go 1.12
 
 require (
+	github.com/gogo/protobuf v1.1.1 // indirect
 	github.com/logrusorgru/aurora v0.0.0-20190428105938-cea283e61946
+	github.com/magefile/mage v1.8.0
 	github.com/mattn/go-isatty v0.0.8
+	github.com/rogpeppe/go-internal v1.3.0
 	github.com/spf13/cobra v0.0.5
+	golang.org/x/crypto v0.0.0-20190313024323-a1f597ede03a // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
 	k8s.io/apimachinery v0.0.0-20190624085041-961b39a1baa0
 	k8s.io/client-go v0.0.0-20190624085356-2c6e35a5b9cf
 	k8s.io/metrics v0.0.0-20190624090658-79adfaea0da2
---
+++ magefile.go
@@ -0,0 +1,92 @@
+// +build mage
+
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	"github.com/magefile/mage/mg"
+
+	"github.com/magefile/mage/sh"
+	"github.com/rogpeppe/go-internal/modfile"
+)
+
+var Default = All
+
+func init() {
+	_ = os.Setenv("GO111MODULE", "on")
+}
+
+// Format, lint, test, and install
+func All() {
+	mg.SerialDeps(Format, Lint, Test, Install)
+}
+
+// Run all linters
+func Lint() error {
+	mg.Deps(Deps)
+	return sh.Run("golangci-lint", "run", "--fix")
+}
+
+// Run goimports
+func Format() error {
+	mg.Deps(Deps)
+	if err := sh.Run("go", "mod", "tidy"); err != nil {
+		return err
+	}
+	module, err := parseGoModule()
+	if err != nil {
+		return err
+	}
+	return sh.Run("goimports", "-l", "-w", "-local", module, ".")
+}
+
+// Installs this module
+func Install() error {
+	return sh.Run("go", "install")
+}
+
+// Test this module
+func Test() error {
+	return sh.Run("go", "test", "-v", "./...")
+}
+
+// Helper to quickly run main
+func Run() error {
+	return sh.RunV("go", "run", "main.go")
+}
+
+// Installs all dependencies for building and testing
+func Deps() error {
+	modules := map[string]string{
+		"golangci-lint": "github.com/golangci/golangci-lint/cmd/golangci-lint",
+		"goimports":     "golang.org/x/tools/cmd/goimports",
+	}
+	for binary, mod := range modules {
+		if !binaryExists(binary) {
+			if err := sh.Run("go", "get", mod); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func parseGoModule() (string, error) {
+	modContents, err := ioutil.ReadFile("go.mod")
+	if err != nil {
+		return "", err
+	}
+	mod, _ := modfile.Parse("", modContents, nil)
+	if err != nil {
+		return "", err
+	}
+	return mod.Module.Mod.Path, nil
+}
+
+func binaryExists(binary string) bool {
+	_, err := exec.LookPath(binary)
+	return err == nil
+}
